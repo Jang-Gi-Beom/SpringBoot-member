10986cf772aaacf3d86ff515aa85526133dc7e03
Jang-Gi-Beom <wkdrlqja1221@naver.com>
2023-05-15 17:35:35 +0900

    member

---
+++ Downloads/member/src/main/java/com/codingrecipe/member/controller/MemberController.java
@@ -0,0 +1,109 @@
+package com.codingrecipe.member.controller;
+
+import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpSession;
+import java.util.List;
+
+@Controller
+@RequiredArgsConstructor
+// final이 붙거나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
+public class MemberController {
+    // 생성자 주입
+    private final MemberService memberService;
+
+    // @@@@@@@ 회원가입 관련
+    @GetMapping("/member/save")
+    public String saveForm() {
+        return "save";
+    }
+
+    @PostMapping("/member/save")
+//    public String save(@RequestParam("memberEmail") String memberEmail,
+//                       @RequestParam("memberPassword") String memberPassword,
+//                       @RequestParam("memberName") String memberName) {
+//        System.out.println("memberEmail = " + memberEmail + ", memberPassword = " + memberPassword + ", memberName = " + memberName);
+//        System.out.println("MemberController.save");
+//        return "index";
+//    }
+
+    public String save(@ModelAttribute MemberDTO memberDTO) {
+        // 매개변수로 MemberDTO로 설정해두면 DTO에 있는 필드와 name속성값이 이름이 같다면 알아서 Setter메소드를 호출 -> 필드에 값을 삽입
+        System.out.println("MemberController.save");
+        System.out.println("memberDTO = " + memberDTO);
+        memberService.save(memberDTO);
+        return "login";
+    }
+
+
+    // @@@@@@@ 로그인 관련
+    @GetMapping("/member/login")
+    public String loginForm() {
+        return "login";
+    }
+
+    @PostMapping("/member/login")
+    public String login(@ModelAttribute MemberDTO memberDTO, HttpSession session) {
+       MemberDTO loginResult =  memberService.login(memberDTO);
+       if (loginResult != null) {
+           // login 성공
+           session.setAttribute("loginEmail",loginResult.getMemberEmail());
+           // 로그인한 회원의 이메일 정보를 session으로 설정
+           return "main";
+       } else {
+           // login 실패
+           return "login";
+       }
+    }
+
+    // @@@@@@@ 회원목록 출력 관련
+    @GetMapping("/member/")
+    public String findAll(Model model) {
+      List<MemberDTO> memberDTOList = memberService.findAll();
+      // 어떠한 html로 가져갈 데이터가 있다면 model사용
+      model.addAttribute("memberList",memberDTOList);
+      return "list";
+    }
+
+    @GetMapping("/member/{id}")
+    public String findById(@PathVariable Long id,Model model) {
+        MemberDTO memberDTO = memberService.findById(id);
+        model.addAttribute("member",memberDTO);
+        return "detail";
+    }
+
+    @GetMapping("/member/update")
+    public String updateForm(HttpSession session, Model model) {
+        String myEmail = (String)session.getAttribute("loginEmail");
+        // session값이 object타입이기 때문에 강제타입변환해야 함
+       MemberDTO memberDTO = memberService.updateForm(myEmail);
+       model.addAttribute("updateMember", memberDTO);
+       return "update";
+
+    }
+
+    @PostMapping("/member/update")
+    public String update(@ModelAttribute MemberDTO memberDTO) {
+        memberService.update(memberDTO);
+        return "redirect:/member/" + memberDTO.getId();
+        // 정보수정을 한 후에 detail을 띄워 줌
+    }
+
+    @GetMapping("/member/delete/{id}")
+    public String deleteById(@PathVariable Long id) {
+        memberService.deleteById(id);
+        return "redirect:/member/";
+    }
+
+    @GetMapping("/member/logout")
+    public String logout(HttpSession session) {
+        session.invalidate();
+        // 세션 삭제
+        return "index";
+    }
+}
---
+++ Downloads/member/src/main/java/com/codingrecipe/member/service/MemberService.java
@@ -0,0 +1,94 @@
+package com.codingrecipe.member.service;
+
+import com.codingrecipe.member.dto.MemberDTO;
+import com.codingrecipe.member.entity.MemberEntity;
+import com.codingrecipe.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    public void save(MemberDTO memberDTO) {
+        // 1. dto -> entity 변환
+        // 2. repository의 save 메서드 호출
+        MemberEntity memberEntity = MemberEntity.toMemberEntity(memberDTO);
+        memberRepository.save(memberEntity);
+        // repository의 save메서드 호출(조건. entity객체를 넘겨줘야 함)
+        // 받아온 entity객체를 repository의 save 메소드를 호출하여 DB에 저장
+
+
+    }
+
+    public MemberDTO login(MemberDTO memberDTO) {
+        /*
+            처리과정
+            1. 회원이 입력한 이메일로 DB에서 조회를 함
+            2. DB에서 조회한 비밀번호와 사용자가 입력한 비밀번호가 일치하는지 판단
+         */
+        Optional<MemberEntity> byMemberEmail = memberRepository.findByMemberEmail(memberDTO.getMemberEmail());
+//        Optional<T>는 null이 올 수 있는 값을 감싸는 Wrapper 클래스로, 참조하더라도 NPE가 발생하지 않도록 도와준다.
+        if (byMemberEmail.isPresent()) {
+            // 조회 결과가 있다(해당 이메일을 가진 회원 정보가 있다)
+           MemberEntity memberEntity = byMemberEmail.get();
+           // get메소드를 이용해서 entity 객체를 가져와서 삽입함
+            if (memberEntity.getMemberPassword().equals(memberDTO.getMemberPassword())) {
+                // 입력한 비번과 DB에 있는 해당 이메일 비번과 맞는지 확인하는 작업
+                // 비밀번호 일치
+                // entity -> dto 객체로 변환 후 리턴
+                MemberDTO dto = MemberDTO.tomemberDTO(memberEntity);
+                return dto;
+
+            } else {
+                // 비밀번호 불일치(로그인 실패)
+                return null;
+            }
+        } else {
+            // 조회 결과 가 없다(해당 이메일을 가진 회원이 없다)
+            return null;
+        }
+    }
+
+    public List<MemberDTO> findAll() {
+        List<MemberEntity> memberEntityList = memberRepository.findAll();
+        List<MemberDTO> memberDTOList = new ArrayList<>();
+        for (MemberEntity memberEntity : memberEntityList) {
+            memberDTOList.add(MemberDTO.tomemberDTO(memberEntity));
+        }
+        return memberDTOList;
+    }
+
+    public MemberDTO findById(Long id) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findById(id);
+        if (optionalMemberEntity.isPresent()) {
+            // Optional의 get메소드를 이용해서 entity 객체를 얻어 옴 이후 dto객체로 변환
+            return MemberDTO.tomemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public MemberDTO updateForm(String myEmail) {
+        Optional<MemberEntity> optionalMemberEntity = memberRepository.findByMemberEmail(myEmail);
+        if (optionalMemberEntity.isPresent()) {
+            return MemberDTO.tomemberDTO(optionalMemberEntity.get());
+        } else {
+            return null;
+        }
+    }
+
+    public void update(MemberDTO memberDTO) {
+        // save : id값이 없으면 insert문 실행 , 값이 있다면 update문 실행
+        memberRepository.save(MemberEntity.toUpdateMemberEntity(memberDTO));
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+}
